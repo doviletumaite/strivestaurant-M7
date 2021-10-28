48baa2bbdac1bf1629667e3b9e9b9fb7290ed98a
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-10-28 16:32:48 +0200

    Update MyNav.tsx

--- src/components/MyNav.tsx
+++ src/components/MyNav.tsx
@@ -1,22 +1,27 @@
-import { Navbar, Nav } from 'react-bootstrap'
-import { Link, withRouter } from 'react-router-dom'
 
-const MyNav = (props) => (
+import { Navbar, Nav } from 'react-bootstrap'
+import { Link, withRouter, RouteComponentProps } from 'react-router-dom'
+
+interface NavProp{
+    title: string
+}
+
+const MyNav = ({location}:RouteComponentProps & NavProp) => (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Link to="/">
-            <Navbar.Brand>{props.title}</Navbar.Brand>
+            <Navbar.Brand title={}></Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
                 <Link to="/">
-                    <div className={props.location.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</div>
+                    <div className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}>Home</div>
                 </Link>
                 <Link to="/menu">
-                    <div className={props.location.pathname === '/menu' ? 'nav-link active' : 'nav-link'}>Menu</div>
+                    <div className={location.pathname === '/menu' ? 'nav-link active' : 'nav-link'}>Menu</div>
                 </Link>
                 <Link to="/reservations">
-                    <div className={props.location.pathname === '/reservations' ? 'nav-link active' : 'nav-link'}>Reservation</div>
+                    <div className={location.pathname === '/reservations' ? 'nav-link active' : 'nav-link'}>Reservation</div>
                 </Link>
             </Nav>
         </Navbar.Collapse>
